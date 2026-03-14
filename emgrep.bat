9b423407aa66ab8fb237842c1d35ac112ac21e4c
safey <155398846+SafeyX3@users.noreply.github.com>
2026-03-13 20:00:07 -0500

    Update emgrep.bat
    original just searched entire tmodloader github, fixed it so it now only searches the ExampleMod

--- emgrep.bat
+++ emgrep.bat
@@ -1,5 +1,5 @@
 @echo off
-set "BEGIN=https://github.com/search?q=repo%%3AtModLoader%%2FtModLoader%%20"
+set "BEGIN=https://github.com/search?q=repo%%3AtModLoader%%2FtModLoader%%20path%%3A%%2F%%5EExampleMod%%5C%%2F%%2F%%20"
 set "END=&type=code"
 set "ARGS=%*"
 set "FULL_LINK=%BEGIN%%ARGS%%END%"
